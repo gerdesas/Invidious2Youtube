f8a63fb2a6e6de17550a2100116a510110d9c51b
Psychotic Ninja Build System <pnbs@psychotic.ninja>
2023-02-07 23:01:15 -0600

    Update copyright date and names

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2019, Peter Ajamian
+# Copyright (c) 2023, Peter Ajamian & John R. Dennison
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
